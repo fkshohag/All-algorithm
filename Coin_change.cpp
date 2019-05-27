1a2467011f98ca5cc2fb19dbef7ca0bd5a3c0b32
MD Shohag <fazlulkabir94@users.noreply.github.com>
2019-05-28 00:43:19 +0600

    Update coin change problem
    With all possible solutions

--- Coin_change.cpp
+++ Coin_change.cpp
@@ -3,15 +3,26 @@
 # include <cstring>
 using namespace std;
 
-int coin[] = {0, 12, 5}, possible[1000];
+int coin[] = {0, 12, 3}, possible[1000];
+
+void solution(int p[]) {
+	int n = 16;
+	for(int i = 0; i < n; i++)
+		printf("%d ", p[i]);
+}
 
 int main() {
 	int n = 15;
 	int len = 2;
 	possible[0] = 1;
-	for(int i = 1; i <= n; i++)
-		for(int j = 1; j <= len; j++)
+	for(int i = 1; i <= n; i++) {
+		for(int j = 1; j <= len; j++) {
 			if(i >= coin[j])
 				possible[i] += possible[i-coin[j]];
+		}
+		solution(possible);
+		printf("\n");
+	}
 	printf("%d\n", possible[15]);
+	return 0;
 }
