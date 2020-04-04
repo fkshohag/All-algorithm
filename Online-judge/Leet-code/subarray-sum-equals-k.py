578e87cadae02ccdad0ebdf7c7ef06df7ca9ca2b
linkvision <linkvisionsoft@yahoo.com>
2020-04-05 03:06:26 +0600

    let code first problem submited

---
+++ Online-judge/Leet-code/subarray-sum-equals-k.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        count = 0
+        for i in range(len(nums)):
+            sum = 0
+            for j in range(i, len(nums)):
+                sum += nums[j]
+                if sum == k:
+                    count += 1
+        return count
