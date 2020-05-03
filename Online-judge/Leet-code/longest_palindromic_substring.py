e8a47e3325276ad8915673ce2068f4bd1affadf3
MD Shohag <fazlulkabir94@users.noreply.github.com>
2020-05-04 01:34:05 +0600

    Create longest_palindromic_substring.py

---
+++ Online-judge/Leet-code/longest_palindromic_substring.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        length = len(s)
+        if length < 0:
+            return s
+        longestString=''
+        longestStringLength = 0
+        for index in range(0, length):
+            left ,right = self.expandPalindrome(s, length, index, index)
+            _left ,_right = self.expandPalindrome(s, length, index, index+1)
+            mx = len(s[left:right])
+            _mx = len(s[_left:_right])
+            if mx > _mx:
+                if mx > longestStringLength:
+                    longestString = s[left:right]
+                    longestStringLength = mx
+            else:
+                if _mx > longestStringLength:
+                    longestString = s[_left:_right]
+                    longestStringLength = _mx
+        return longestString
+
+    def expandPalindrome(self, word, length, left, right):
+        _left, _right = left, right
+        while _left >= 0 and _right < length and word[_left] == word[_right]:
+            _left -= 1
+            _right += 1
+        return _left+1, _right
