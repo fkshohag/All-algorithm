264afe7fc46785529f2d23286d70ac20037dd8ea
MD Shohag <fazlulkabir94@users.noreply.github.com>
2020-05-06 02:50:56 +0600

    Create longest_palindromic_substring_dp.py

---
+++ Online-judge/Leet-code/longest_palindromic_substring_dp.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        length = len(s)
+        _solution = ''
+        if length < 2:
+            return s
+        dp = [[False for i in range(length+1)] for j in range(length+1)]
+        for i in range(length):
+            for j in range(i,-1,-1):
+                if s[j] == s[i]:
+                    dp[i][j] = dp[i-1][j+1] if i-j > 2 else True
+                if dp[i][j]:
+                    if len(_solution) < (i-j+1):
+                        _solution = s[j:i+1]
+        return _solution
