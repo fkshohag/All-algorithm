7751ac66ef18c62fae36c0dd4276089db9395fa1
MD Shohag <fazlulkabir94@users.noreply.github.com>
2020-05-15 00:57:26 +0600

    quick sort algorithm implemented

---
+++ Sorting-algorithm/quick_sort.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func partition(list []int, low int, high int) (int, []int) {
+	counter, pivot := low-1, list[high-1]
+	for i := low; i < high; i++ {
+		if list[i] < pivot {
+			counter++
+			list[counter], list[i] = list[i], list[counter]
+		}
+	}
+	list[counter+1], list[high-1] = list[high-1], list[counter+1]
+	return counter + 1, list
+}
+
+func quickSort(list []int, low int, high int) []int {
+	if low < high {
+		mid, list := partition(list, low, high)
+		quickSort(list, low, mid-1)
+		quickSort(list, mid+1, high)
+	}
+	return list
+}
+
+func main() {
+	var list = []int{1, -34, 4, 34, -3, 43, 5, 56, 3}
+	result := quickSort(list, 0, len(list))
+	fmt.Println(result)
+}
