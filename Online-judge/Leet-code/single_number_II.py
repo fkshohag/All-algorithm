822156e686f0b9a592fcfdca38362b2607fe3517
MD Shohag <fazlulkabir94@users.noreply.github.com>
2020-06-20 04:32:44 +0600

    update two time

---
+++ Online-judge/Leet-code/single_number_II.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def singleNumber(self, nums):
+    	nums_dist = {};
+        for num in nums:
+        	if num not in nums_dist:
+        	 	nums_dist[num] = 1
+        	else:
+        		nums_dist[num] += 1
+
+        for index, value in nums_dist.items():
+        	if value == 1:
+        		return index
