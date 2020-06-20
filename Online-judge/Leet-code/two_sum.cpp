7bf44dc4d1c83c9bedfc0c6965abb9a53cfd9562
MD Shohag <fazlulkabir94@users.noreply.github.com>
2020-06-20 16:21:53 +0600

    update

--- Online-judge/Leet-code/two_sum.cpp
+++ Online-judge/Leet-code/two_sum.cpp
@@ -9,15 +9,15 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-		vector<int> res;
+	vector<int> res;
         for(int i = 0; i < nums.size(); i++) {
-			for(int j = i+1; j < nums.size(); j++) {
-				if(nums[i] + nums[j] == target) {
-					res.push_back(i);
-					res.push_back(j);
-				}
+		for(int j = i+1; j < nums.size(); j++) {
+			if(nums[i] + nums[j] == target) {
+				res.push_back(i);
+				res.push_back(j);
 			}
 		}
-		return res;
+	}
+	return res;
     }
 };
