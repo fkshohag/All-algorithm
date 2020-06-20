16ba355a2210058539b665d0a7d08ba85bef3824
MD Shohag <fazlulkabir94@users.noreply.github.com>
2020-06-20 19:55:49 +0600

    Create longest-common-prefix.cpp

---
+++ Online-judge/Leet-code/longest-common-prefix.cpp
@@ -0,0 +1,44 @@
+# include <iostream>
+# include <cstdio>
+# include <vector>
+# include <algorithm>
+# include <map>
+# include <cstring>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+		string res;
+		if(strs.size() < 1) return res;
+        if(strs.size() == 1) return strs[0];
+        sort(strs.begin(), strs.end(), [](const string& first, const string& second){
+			return first.length() < second.length();
+		});
+		bool flag = false;
+		int index = 0;
+		for(int i = 0; i < strs[0].length(); i++) {
+			for(int j = 1; j < strs.size(); j++) {
+				if(strs[j][i] != strs[0][i]) {
+					flag = true;
+					break;
+				}
+				if(j == strs.size()-1) index++;
+			}
+			if(flag) break;
+		}
+		if(flag && !index) return res;
+		return strs[0].substr(0, index);
+    }
+};
+
+int main()
+{
+	vector<string> v;
+	v.push_back("aca");
+	v.push_back("cba");
+	Solution s;
+	string res = s.longestCommonPrefix(v);
+	cout << res;
+}
