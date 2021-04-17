cb9331a86f83de5c3a4abaa02f614248c6dc1947
Shohag <shohag.fks@gmail.com>
2021-04-17 23:15:03 +0600

    solver added

--- academic/statistics/max_min_data_set.py
+++ academic/statistics/max_min_data_set.py
@@ -1,16 +1,18 @@
 class Solver(object):
     def findMin(self, data_set):
-        return (data_set)
+            print(min(data_set))
 
     def findMax(self, data_set):
-        return max(data_set)
+        print(max(data_set))
     
     def sortedData(self, data_set):
-        return data_set.sorted()
+        x = sorted(data_set)
+        print(x)
 
 s = Solver()
-data_set = []
-mi = s.findMin(data_set)
-mx = s.findMax(data_set)
-data_set = s.sortedData(data_set)
-print(mi, mx, data_set)
+data_set = [8, 25, 11, 15, 29, 22, 10, 5, 17, 21,
+22, 13, 26, 16, 20, 12, 9, 26, 20, 16,
+23, 14, 19, 23, 20, 16, 27, 16, 21, 14]
+s.findMin(data_set)
+s.findMax(data_set)
+s.sortedData(data_set)
