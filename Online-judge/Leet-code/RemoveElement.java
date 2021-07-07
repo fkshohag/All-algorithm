4c397fc2011dbebe3e7592d455946c367211f30f
MD Shohag <8865985+fkshohag@users.noreply.github.com>
2021-07-07 16:11:54 +0600

    Create RemoveElement.java

---
+++ Online-judge/Leet-code/RemoveElement.java
@@ -0,0 +1,23 @@
+package Letcode;
+
+/**
+ * @author Md.Shohag <shohag.fks@gmail.com>
+ * Problem : https://leetcode.com/problems/remove-element
+ */
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int newLenght = -1;
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != val) {
+                nums[++newLenght] = nums[index];
+            }
+        }
+        return newLenght + 1;
+    }
+    public static void main(String[] args) {
+        RemoveElement r = new RemoveElement();
+        int []element = {0,1,2,2,3,0,4,2};
+        int re = r.removeElement(element, 2);
+        System.out.println(re);
+    }
+}
