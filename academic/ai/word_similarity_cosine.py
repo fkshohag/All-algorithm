763fa0260519f94bbaf499c64e4578e93142e6f0
Shohag <shohag.fks@gmail.com>
2021-08-18 17:19:02 +0600

    dot method aded

--- academic/ai/word_similarity_cosine.py
+++ academic/ai/word_similarity_cosine.py
@@ -13,11 +13,17 @@ def product_of_magnitude(vec):
         total += (x * x)
     return np.sqrt(total)
 
+def dot_product(vec1, vec2):
+    return np.dot(vec1, vec2)
+
 def cosine_similarity(vec1, vec2):
     return np.dot(vec1, vec2) / (np.sqrt(np.dot(vec1, vec1)) * np.sqrt(np.dot(vec2, vec1)))
 
-x = np.array([[2, 1, 0, 2, 0, 1, 1, 1], [2, 1, 1, 1, 1, 0, 1, 1]])
-x0 = x[0][:-1]
-x1 = x[1][:-1]
+x = np.array([[2,4,4,5], [0,4,3,0]])
+x0 = x[0]
+x1 = x[1]
+print(x0)
+print(x1)
 print("x0 and x1:", eucledian_distance(x0, x1))
-print("x0 and x1:", cosine_similarity(x0, x1))
+print("cosine x0 and x1:", cosine_similarity(x0, x1))
+print("dot:", dot_product(x0,x1))
