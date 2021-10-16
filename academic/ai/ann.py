d98d9183c24795160e2b0f62ef8e770d7dc6c15b
Shohag <shohag.fks@gmail.com>
2021-10-17 03:21:06 +0600

    single perception added

--- academic/ai/ann.py
+++ academic/ai/ann.py
@@ -23,19 +23,25 @@ def setp(yp):
 
 epoch = 1
 
+print("ep", "x1", "x2", "yd", "w1", "w2", "yp", 'er', "w1", "w2")
+print("\n")
+
 while True:
     for i in range(4):
         yp = x1[i] * w1 + x2[i] * w2 - th
         yp = setp(yp)
         errors[i] = yd[i] - yp
+        w1i = w1
+        w2i = w2
 
         w1 += lr * x1[i] * errors[i]
         w2 += lr * x2[i] * errors[i]
+
+        print(epoch, x1[i], x2[i], yd[i], round(w1i, 2), round(w2i, 2), round(yp, 2), errors[i] ,round(w1, 2),  round(w2, 2))
 
     result = checkForError(errors)
 
     if result is True:
         break
     epoch += 1
-
-print("epoch= ", epoch, "and weight w1 = ", w1, "w2 =", w2)
+    print("\n")
