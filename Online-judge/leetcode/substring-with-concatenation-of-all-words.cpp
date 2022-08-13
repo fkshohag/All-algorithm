7cc0ef060c2f4fbdae2fe1be3c9167488bb44f87
Shohag <shohag.fks@gmail.com>
2022-08-14 04:37:21 +0600

    concatenation added

---
+++ Online-judge/leetcode/substring-with-concatenation-of-all-words.cpp
@@ -0,0 +1,51 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * @author Md.Shohag <shohag.fks@gmail.com>
+ * Problem : https://leetcode.com/problems/substring-with-concatenation-of-all-words/
+ */
+
+class Solution {
+private:
+    unordered_map<string, int>wordFrequency;
+    int wordsSize;
+    int wSize;
+    int subStringSize;
+    
+    bool isValidSubString(unordered_map<string, int>wf, int currentIndex, string s) {
+        int matchSize = 0;
+        for (int index = currentIndex; index < currentIndex + subStringSize; index += wSize) {
+            string sub = s.substr(index, wSize);
+            if (wf.find(sub) != wf.end() && wf[sub] != 0) {
+                wf[sub]--;
+                matchSize++;
+            }
+            else {
+                break;
+            }
+        }
+        return wordsSize == matchSize;
+    }
+    
+    
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        wordsSize = words.size();
+        wSize = words[0].size();
+        subStringSize = wordsSize * wSize;
+        
+        for (string word : words) {
+            wordFrequency[word]++;
+        }
+        
+        vector<int>ans;
+        
+        for (int index = 0; index < s.size() - subStringSize + 1; index++) {
+            if (isValidSubString(wordFrequency, index, s)) {
+                ans.push_back(index);
+            } 
+        }
+        return ans;
+    }
+};
