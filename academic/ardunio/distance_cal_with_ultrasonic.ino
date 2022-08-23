2112488770e35c99a147f77fb4d512a7b1ef19b0
Shohag <shohag.fks@gmail.com>
2022-08-24 02:35:18 +0600

    fix: explantion added for distance calculation

--- academic/ardunio/distance_cal_with_ultrasonic.ino
+++ academic/ardunio/distance_cal_with_ultrasonic.ino
@@ -1,3 +1,17 @@
+/*
+Explanation:
+0 degree tempature sound velocity             = 332 m/s
+1 degree increase tempature velocity increase = 0.6 m/s
+33 degree                                     = 332 + (6 * 33)
+                                              = 3518 m/s
+Romm tempature = 35
+1 s = 3518 msc;
+x = ((3518) * x) / 10 ^ 6
+x = 0.00003518 m
+x = 0.03518 s
+*/
+
+
 int trigPin = 8, echoPin = 9;
 float distance, duration;
 
