c2a45d2c52d29c4c8fbdb92b820145488f3c9675
Shohag <shohag.fks@gmail.com>
2022-09-25 00:44:21 +0600

    delete node problem added

---
+++ Online-judge/leetcode/delete-node-in-a-linked-list.cpp
@@ -0,0 +1,24 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * @author Md.Shohag <shohag.fks@gmail.com>
+ * Problem : https://leetcode.com/problems/delete-node-in-a-linked-list/
+ * O(n)
+*/
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* nextNode = node->next;
+        node->val = nextNode->val;
+        node->next = nextNode->next;
+        nextNode->next = nullptr;
+    }
+};
