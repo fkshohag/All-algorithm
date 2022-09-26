86a3a5ba191469eeff8ae0b232740042f27ca90f
Shohag <shohag.fks@gmail.com>
2022-09-27 03:45:51 +0600

    bfs solution added

--- Online-judge/leetcode/minimum-depth-of-binary-tree.cpp
+++ Online-judge/leetcode/minimum-depth-of-binary-tree.cpp
@@ -7,8 +7,6 @@ using namespace std;
  * Running complexity O(n)
 */
 
-
- 
  struct TreeNode {
     int val;
     TreeNode *left;
@@ -17,6 +15,41 @@ using namespace std;
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
+// Bfs solution
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root) return 0;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        int ans = 1;
+        
+        while(!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                
+                if (!node->left && !node->right) {
+                    return ans;
+                }
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+            ans++;
+        }
+        return ans;
+    }
+};
+
+// Recursion solution
 
 class Solution {
 public:
