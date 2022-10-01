cd7ac0127a6dc5bc242d0ce09dcc788a32315c1f
Shohag <shohag.fks@gmail.com>
2022-10-01 11:35:06 +0600

    palindome code added

--- academic/micro-processor/palindome.asm
+++ academic/micro-processor/palindome.asm
@@ -1,1 +1,28 @@
 CODE SEGMENT
+     ASSUME CS:CODE DS:CODE
+
+     MOV AX, 8301H
+     PUSH AX
+     XOR BX, BX
+
+     MOV CL, 16D
+
+     L1:
+       SHR AX, 1
+       ADC BX, 0H
+       CMP CX, 1
+       JE Last
+       SHL BX, 1
+       LOOP L1
+
+    LAST:
+       POP AX
+       CMP AX, BX
+       JE EQUAL
+       MOV DX, 1
+           HLT 
+    EQUAL:
+         MOV DX, 0
+         HLT 
+    CODE ENDS 
+         END
