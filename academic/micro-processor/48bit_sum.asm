590680f687865f1a572846c99ea14512059189da
Shohag <shohag.fks@gmail.com>
2022-10-01 12:06:05 +0600

    48bit sum

---
+++ academic/micro-processor/48bit_sum.asm
@@ -0,0 +1,27 @@
+CODE SEGMENT
+     ASSUME CS:CODE, DS:CODE
+
+     LEA SI, A 
+     LEA DI, B 
+     LEA  DX, OFFSET BX
+
+     MOV AX, [SI, + 4]
+     ADD AX, [DI + 4]
+     MOV [BX + 4], AX
+
+     MOV AX, [SI + 2]
+     ADD AX, [DI + 2]
+     MOV [BX+2], AX
+
+     MOV AX, [SI]
+     ADD AX, [DI]
+     MOV [BX], AX
+
+     HLT 
+
+A DW 1234H, 0ABCDH, 5768H
+B DW 1234H, 0ABCDH, 5768H
+S DW 0H, 0H, 0H
+
+CODE ENDS
+     END
