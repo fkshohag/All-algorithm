fb479d5cfa606b8c6370cb396a64f4feddebd2b0
Shohag <shohag.fks@gmail.com>
2022-10-07 04:53:12 +0600

    fix: subset

---
+++ Online-judge/leetcode/subsets.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * @author Md.Shohag <shohag.fks@gmail.com>
+ * Problem : https://leetcode.com/problems/subsets/
+ */
+
+
+
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void solve(int index, vector<int>& nums, vector<int>v) {
+        if (index == nums.size()) {
+            ans.push_back(v);
+            return;
+        }
+        solve(index + 1, nums, v); // exclude
+        v.push_back(nums[index]); // take
+        solve(index + 1, nums, v); // next
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        solve(0, nums, vector<int> {});
+        return ans;
+    }
+};
