4133d152d9eb598edaf21c9125497f7653e77945
Shohag <shohag.fks@gmail.com>
2022-12-06 12:17:40 +0600

    fix: odd even list

---
+++ Online-judge/leetcode/odd-even-linked-list.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * @author Md.Shohag <shohag.fks@gmail.com>
+ * Problem : https://leetcode.com/problems/odd-even-linked-list/description/
+ * Time complexity: O(n).
+ * Space complexity: O(1) we just took four pointer.
+ */
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (!head) return NULL;
+        ListNode *odd = head, *even = head->next, *evenHead = even;
+
+        while (even && even->next) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        
+        odd->next = evenHead;
+        return head;
+    }
+};
