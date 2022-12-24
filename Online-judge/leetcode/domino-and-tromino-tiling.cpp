74124ce0f653a6f81ccde8c4f5170dd905836c5c
Shohag <shohag.fks@gmail.com>
2022-12-25 02:20:55 +0600

    longest common subsequence

--- Online-judge/leetcode/domino-and-tromino-tiling.cpp
+++ Online-judge/leetcode/domino-and-tromino-tiling.cpp
@@ -16,9 +16,8 @@ class Solution {
 public:
     int numTilings(int n) {
         vector<ll> dp(1000+1, 0);
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 5;
+        dp[1] = 1; dp[2] = 2; dp[3] = 5;
+
         if (n < 4) return dp[n];
         for (int i = 4; i <= n; i++) {
             dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD;
---
+++ Online-judge/leetcode/longest-common-subsequence.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * @author Md.Shohag <shohag.fks@gmail.com>
+ * Problem : https://leetcode.com/problems/longest-common-subsequence/description/
+ * Time Complexity: O(M*N)
+ * Space Complexity: O(M*N)
+ */
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int dp[text1.size() + 1][text2.size() + 1];
+        memset(dp, 0, sizeof(dp));
+
+        for (int row = text1.size()-1; row >= 0; row--) {
+            for (int col = text2.size()-1; col >= 0; col--) {
+                if (text1[row] == text2[col]) {
+                    dp[row][col] = 1+dp[row+1][col+1]; // character match that means need to move diagonal
+                } else {
+                    dp[row][col] = max(dp[row+1][col], dp[row][col+1]); // not match take max from right / bottom
+                }
+            }
+        } 
+        return dp[0][0];
+    }
+};
