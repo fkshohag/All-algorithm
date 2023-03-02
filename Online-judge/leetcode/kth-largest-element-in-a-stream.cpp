8102b0ce2204b576bd451353f515b50e0608c98b
Shohag <shohag.fks@gmail.com>
2023-03-02 14:40:29 +0600

    kth largest element

---
+++ Online-judge/leetcode/kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * @author Md.Shohag <shohag.fks@gmail.com>
+ * Problem : https://leetcode.com/problems/kth-largest-element-in-a-stream/description/
+ * Complexity: running O(nlogn)
+ */
+
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int K;
+    
+    KthLargest(int k, vector<int>& nums) {
+        K = k;
+        for(int x : nums) {
+            pq.push(x);
+            if(pq.size() > k) {
+                pq.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if(pq.size() > K) {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
