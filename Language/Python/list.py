2e40b1cd9f4074a33926035d423881631598e540
Shohag <shohag.fks@gmail.com>
2023-03-03 00:25:06 +0600

    fix: python program updated

---
+++ Language/Python/list.py
@@ -0,0 +1,15 @@
+l = [1, 2, 3]
+l.append(4)
+l.insert(0, 0)
+l.pop(0)
+_len = len(l)
+
+print(_len)
+
+l2 = [2, 3, 4]
+l3 = l+l2
+l3.sort()
+print(l3)
+
+print(l3[1:-1])
+
---
+++ Language/Python/queue.py
@@ -0,0 +1,6 @@
+from queue import PriorityQueue
+
+q = PriorityQueue()
+q.put((1, 2))
+q.put((2, 3))
+print(q)
