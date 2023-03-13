5b2be7559cff9ce00af4c3dd5ee690c3bc82487f
Shohag <shohag.fks@gmail.com>
2023-03-14 03:05:59 +0600

    fix: kth missing positive integer

---
+++ Online-judge/leetcode/kth-missing-positive-number.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * @author Md.Shohag <shohag.fks@gmail.com>
+ * Problem : https://leetcode.com/problems/kth-missing-positive-number/description/
+ */
+
+/**
+ * @brief 
+ * Time complexity : O(logn)
+ * Space complexity: O(1)
+ */
+
+
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int left = 0, right = arr.size() - 1;
+
+        while (left <= right) {
+            int pivot = left + (right - left) / 2;
+            if (arr[pivot] - pivot - 1 < k) {
+                left = pivot + 1;
+            } else {
+                right = pivot - 1;
+            }
+        }
+        return left + k;
+    }
+};
