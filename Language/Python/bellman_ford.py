9b0eacce0e2a664105a8446172f7677d0cb6dbe9
Shohag <shohag.fks@gmail.com>
2023-04-11 06:14:10 +0600

    fix: bellman for algorithm

---
+++ Language/Python/bellman_ford.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        dist = [0] + [float("inf")] * N
+        dist[K] = 0
+        for node in range(1, N):
+            for u,v,w in times:
+                if dist[u] + w < dist[v]:
+                    dist[v] = dist[u] + w
+        if max(dist) < float("inf"):
+            return max(dist)
+        else:
+            return -1
