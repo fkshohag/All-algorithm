1677541a632e5db0ed9ad3c6e8edb1801749f288
Shohag <shohag.fks@gmail.com>
2024-05-01 01:23:06 +0600

    comment

--- Codinginterview/essent/p1-with-out-sub-query.sql
+++ Codinginterview/essent/p1-with-out-sub-query.sql
@@ -1,3 +1,15 @@
+`
+In this query:
+
+p1 represents the current day's data.
+p2 represents the previous day's data.
+p3 represents the next day's data.
+We join the prices table three times: once to represent the current day, once to represent the previous day (p2), and once to represent the next day (p3). Then, we compare the prices of the current day with the prices of the previous and next days using the join conditions. Finally, we add additional conditions to exclude the first day and the last day from the result set.
+
+This approach eliminates the need for subqueries and may improve the query's performance, especially on large datasets.
+`
+
+
 SELECT p1.day
 FROM prices p1
 JOIN prices p2 ON p1.day = p2.day - 1
