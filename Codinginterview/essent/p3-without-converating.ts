9bbdca93f622ab89c920509cc40b711fcbebe150
Shohag <shohag.fks@gmail.com>
2024-05-01 15:12:42 +0600

    fix: time complexity

--- Codinginterview/essent/p3-without-converating.ts
+++ Codinginterview/essent/p3-without-converating.ts
@@ -1,3 +1,24 @@
+/*
+
+Let's analyze the time complexity of the updated countNumberOfShips function using Depth-First Search (DFS):
+
+Iterating over the Board: The function iterates over each cell of the board, which is an N x M grid, 
+where N is the number of rows and M is the number of columns. So, the time complexity for this step is O(N * M).
+DFS for Each Ship:
+For each cell representing the start of a ship (value is 1), the getSize function performs DFS to 
+explore the connected ship cells.
+In the worst case, each ship can have at most N * M cells.
+The DFS operation for each ship can visit each cell at most once, marking it as visited. Therefore, 
+the time complexity for DFS in each ship is O(N * M).
+Overall Time Complexity:
+Since step 1 and step 2 are nested, the overall time complexity is the product of their complexities.
+So, the overall time complexity is O(N * M * N * M), which simplifies to O(N^2 * M^2).
+Given the size of the input grid, this algorithm also has polynomial time complexity similar to the BFS version. 
+It may perform efficiently for small to moderate-sized grids but may become slower for large grids due to its quadratic 
+complexity.
+*/
+
+
 function countNumberOfShips(board: string[]): number[] {
     const numRows = board.length;
     const numCols = board[0].length;
@@ -48,6 +69,15 @@ function countNumberOfShips(board: string[]): number[] {
     }
 
     // O(m * n)
+    /*
+    Stack Space for DFS: For each DFS call, memory is allocated on the call stack to store the function call and 
+    local variables. Since we perform DFS on each cell, the maximum depth of the call stack can be equal to 
+    the number of cells in the largest ship. In the worst case, the largest ship can cover the entire grid, 
+    so the maximum stack depth is O(N * M).
+    Auxiliary Space: Apart from the call stack, additional space is used to store the grid representation. However, 
+    this space is negligible compared to the stack space and can be considered constant.
+    */
+   
     function getSizeWithDfs(coord: [number, number]): number {
         const [row, col] = coord;
         if (row < 0 || row >= numRows || col < 0 || col >= numCols || grid[row][col] === 0) {
